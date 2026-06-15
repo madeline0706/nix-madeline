fe36f0ac7c6d8d5e51e361de58172799c475ef99
Madeline Barnes <madelinebarnes0706@gmail.com>
2026-06-15 08:58:51 -0700

    test power management settings

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,29 @@
   hardware.cpu.amd.updateMicrocode = true;
   hardware.enableRedistributableFirmware = true;
 
+  # Power management
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      START_CHARGE_THRESH_BAT0 = 40;
+      STOP_CHARGE_THRESH_BAT0 = 80;
+    };
+  };
+
+  services.power-profiles-daemon.enable = false;
+
+  services.logind.settings.Login = {
+    HandleLidSwitch = "suspend";
+    HandleLidSwitchExternalPower = "lock";
+    HandleLidSwitchDocked = "ignore";
+  };
+
   # Networking
   networking.hostName = "arcanine-nix";
   networking.networkmanager.enable = true;
