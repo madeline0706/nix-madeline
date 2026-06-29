df96371e732b6d8038ddf764fe160574d76f59be
Madeline Barnes <madelinebarnes0706@gmail.com>
2026-06-28 17:47:32 -0700

    freakmode

--- hosts/bulbasaur-nix/configuration.nix
+++ hosts/bulbasaur-nix/configuration.nix
@@ -3,21 +3,17 @@
   imports = [
     ./hardware-configuration.nix
   ];
-
   home-manager.users.madeline.imports = [ ./displays.nix ];
-  # Stuff for my Pi 5
-  boot.binfmt.emulatedSystems = [ "aarch64-linux" ]; #to build pi 5 image
-  nix.settings.trusted-users = [ "madeline" ];
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+  nix.settings.extra-platforms = [ "aarch64-linux" ];
+  nix.settings.trusted-users = [ "madeline" ];
+  nix.settings.extra-substituters = [ "https://nixos-raspberrypi.cachix.org" ];
+  nix.settings.extra-trusted-public-keys = [ "nixos-raspberrypi.cachix.org-1:4iMO9LXa8BqhU+Rpg6LQKiGa2lsNh/j2oiYLNOQ5sPI=" ];
   networking.hostName = "bulbasaur-nix";
-
   programs.steam.enable = true;
-
-  # DRM Log noise in Ly
   boot.consoleLogLevel = 3;
-  # AMD GPU
   hardware.graphics = {
     enable = true;
     enable32Bit = true;
