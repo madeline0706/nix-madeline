0bc3b371e75eea6610588453de5aa46ccbb3b6c6
Madeline Barnes <madelinebarnes0706@gmail.com>
2026-08-04 07:34:14 -0700

    Update config

--- parts/system/common.nix
+++ parts/system/common.nix
@@ -35,9 +35,10 @@
         git push
       }
       mc() {
-        local version
+        local version channel='Release'
+        [ "$1" = "--snapshots" ] && channel='Release|Snapshot'
         version=$(portablemc search 2>/dev/null \
-          | awk -F'│' 'NF>3 && $3 ~ /Release/ {gsub(/^[ \t]+|[ \t]+$/,"",$2); print $2}' \
+          | awk -F'│' -v ch="$channel" 'NF>3 && $3 ~ ch {gsub(/^[ \t]+|[ \t]+$/,"",$2); print $2}' \
           | bemenu -l 20 -p "minecraft" --fn 'Terminus 12' -c --width-factor 0.3 \
               --nb '#000000ff' --hb '#000000ff' --fb '#000000ff' --ab '#000000ff' \
               --tb '#000000ff' --tf '#a7c080ff' --ff '#c8c4b0ff' --hf '#dbbc7fff' \
